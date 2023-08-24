27d269dba7bb2f48f9712e8c9bf94a5e27f115c4
FabienTavernier <fabien.tavernier@oclock.io>
2023-08-24 10:21:03 +0200

    1. Statics components: SearchBar

--- src/components/App/SearchBar.tsx
+++ src/components/App/SearchBar.tsx
@@ -1,5 +1,21 @@
+import { Form, Input, Segment } from 'semantic-ui-react';
+
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <Segment>
+      <Form role="search">
+        <Form.Field>
+          <Input
+            icon="search"
+            iconPosition="left"
+            placeholder="Rechercher un dépôt…"
+            aria-label="Rechercher un dépôt"
+            role="searchbox"
+          />
+        </Form.Field>
+      </Form>
+    </Segment>
+  );
 }
 
 export default SearchBar;
