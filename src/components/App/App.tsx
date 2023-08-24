efcd74768b646dd254921d16d8a5bd5fca3d6573
FabienTavernier <fabien.tavernier@oclock.io>
2023-08-24 09:15:12 +0200

    0. Install Semantic UI

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,11 @@
-import logo from '../../assets/logo.svg';
+import { Button } from 'semantic-ui-react';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Click Here</Button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/App/App';
 
+import 'semantic-ui-css/semantic.min.css';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
