c023bf8eeb7f2274461a143daa0513eafd8a85e1
FabienTavernier <fabien.tavernier@oclock.io>
2023-08-24 10:24:16 +0200

    1. Statics components: Message

--- src/components/App/Message.tsx
+++ src/components/App/Message.tsx
@@ -1,5 +1,13 @@
+// Message est déjà pris par mon composant,
+// je renomme mon import grâce à `as`
+import { Message as MessageSemanticUI } from 'semantic-ui-react';
+
 function Message() {
-  return <div>Message</div>;
+  return (
+    <MessageSemanticUI>
+      La recherche a retourné XXXXX résultats
+    </MessageSemanticUI>
+  );
 }
 
 export default Message;
