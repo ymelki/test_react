e057326efba96883294c06c39e846b9e6515be38
FabienTavernier <fabien.tavernier@oclock.io>
2023-08-24 14:16:17 +0200

    4. Form submit: display data from API response

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 import Message from './Message';
 import ReposResults from './ReposResults';
 
-import repos from '../../data/repos';
+// import repos from '../../data/repos';
 import logo from '../../assets/images/logo-github.png';
 
 import './App.scss';
@@ -14,16 +14,29 @@ import './App.scss';
 function App() {
   // État : est-ce que le formulaire est soumis ?
   const [isSubmit, setIsSubmit] = useState(false);
+  // État : pour stocker les résultats de l'API
+  const [total, setTotal] = useState(0);
+  const [repos, setRepos] = useState([]);
 
   useEffect(() => {
-    console.log('USEEFFECT');
     async function getRepos() {
-      console.log('APPEL API');
       try {
-        const response = await axios.get(
+        // const response = await axios.get(
+        //   'https://api.github.com/search/repositories?q=react'
+        // );
+        // console.log(response);
+
+        // Axios retourne TOUJOURS le résultat de l'API
+        // dans un objet avec la propriété `data`
+        // on peut directement décomposer `response`
+        // et assigner les résultats dans `data`
+        const { data } = await axios.get(
           'https://api.github.com/search/repositories?q=react'
         );
-        console.log(response);
+        // console.log(data);
+        // on stocke les résultats dans nos variable d'état
+        setTotal(data.total_count);
+        setRepos(data.items);
       } catch (error) {
         console.error(error);
       } finally {
@@ -47,8 +60,8 @@ function App() {
       </header>
 
       <SearchBar setIsSubmit={setIsSubmit} />
-      <Message total={repos.total_count} />
-      <ReposResults list={repos.items} />
+      <Message total={total} />
+      <ReposResults list={repos} />
     </div>
   );
 }
